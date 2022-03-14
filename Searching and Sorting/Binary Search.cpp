468271d5843c03b87efab748444af278b43e8639
Ayush Katiyar <89083667+ayushkxyz@users.noreply.github.com>
2022-03-14 23:23:47 +0530

    Create Binary Search.cpp

---
+++ Searching and Sorting/Binary Search.cpp
@@ -0,0 +1,19 @@
+int binarySearch(int *input, int n, int val)
+{
+    //Write your code here
+    int start=0;
+    int end=n-1;
+    while(start<=end){
+        int mid=(start+end)/2;
+        if(input[mid]==val){
+            return mid;
+        }
+        else if(input[mid]>val){
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+    }
+    return -1;
+}
