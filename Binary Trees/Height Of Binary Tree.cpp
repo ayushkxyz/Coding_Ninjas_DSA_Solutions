1bef5cb7455e37d1a7e2f2599689a47c434fe0b6
Ayush Katiyar <89083667+ayushkxyz@users.noreply.github.com>
2022-06-20 19:34:53 +0530

    Create Height Of Binary Tree.cpp

---
+++ Binary Trees/Height Of Binary Tree.cpp
@@ -0,0 +1,8 @@
+int height(BinaryTreeNode<int>* root) {
+    if(root==NULL){
+        return 0;
+    }
+    return 1+max(height(root->left),height(root->right));
+    
+    // Write our code here
+}
